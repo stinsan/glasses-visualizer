6457862e3c41562c96cfed9e0bb4066d28b9a73a
Quyen <qhttran1028@gmail.com>
2020-04-16 20:21:45 -0500

    the prototypical look of the overall ui

--- ui.py
+++ ui.py
@@ -18,8 +18,9 @@ class Button:
         self.height = height
         self.color = color
         self.text = text        # Default no text.
+        select = False          # button's state of being selected
 
-    def draw(self, window):
+    def draw(self, font_size, window):
         """ Draws the button onto the window.
         :param window: The surface we want to draw the button on.
         """
@@ -31,7 +32,7 @@ class Button:
 
         # Draw text
         if self.text != '':
-            font = pygame.font.SysFont('consolas', 24)                      # Consolas font of size 24.
+            font = pygame.font.SysFont('consolas', font_size)                      # Consolas font of size 24.
             text = font.render(self.text, 1, (0, 0, 0))                     # Render font black.
             center = (self.x + (self.width / 2 - text.get_width() / 2),     # Center of button.
                       self.y + (self.height / 2 - text.get_height() / 2))
@@ -49,6 +50,16 @@ class Button:
                 return True
 
         return False
+
+    def setColor(self, color):
+        self.color = color
+
+    def setSelect(self, select):
+        self.select = select
+
+    # def getSelect(self):
+    #     return self.select
+
 
 
 def monodepth(filename, model_name):
@@ -88,14 +99,36 @@ def upload_btn_handler(surface):
 
 if __name__ == '__main__':
     pygame.init()  # Initialize PyGame.
-    main_surf = pygame.display.set_mode((1024, 676))
+    main_surf = pygame.display.set_mode((1200, 676))
+    main_surf.fill((53,105,69)) # gray moss green background
+
+    # Set dimension for screen, where image is display
+    screen = main_surf.subsurface(Rect(200, 90, 800, 500))
+    screen.fill((197,222,204))
 
     pygame.display.set_caption('Glasses Visualizer')   # Name of the window.
-    upload_btn = Button(0, 630, 100, 30, (0, 255, 0), 'Upload')   # Initialize the upload button.
+
+    # Bottom of Screen
+    upload_btn = Button(550, 600, 120, 35, (191, 117, 105), 'UPLOAD')   # Initialize the upload button.
+        # Left of Screen
+    rg_btn = Button(30, 200, 150, 50, (255, 255, 255), 'R/G Colorblindness')    # Initialize the rg colorblind button.
+    by_btn = Button(30, 270, 150, 50, (255, 255, 255), 'B/Y Colorblindness')    # Initialize the yg colorblind button.
+    total_btn = Button(30, 340, 150, 50, (255, 255, 255), 'Total Colorblindness')   # Initialize the total colorblind button.
+        # Right of Screen
+    myopia_btn = Button(1020, 200, 150, 50, (255, 255, 255), 'Myopia')   # Initialize the upload button.
+    hyperopia_btn = Button(1020, 270, 150, 50, (255, 255, 255), 'Hyperopia')   # Initialize the upload button.
+    glasses_btn = Button(1020, 340, 150, 50, (255, 255, 255), 'Glasses On')  # Initialize the glass button.
+
     is_running = True
 
     while is_running:
-        upload_btn.draw(main_surf)  # Draw the upload button.
+        upload_btn.draw(20, main_surf)  # Draw the upload button.
+        rg_btn.draw(12, main_surf)      # Draw the red-green colorblind button.
+        by_btn.draw(12, main_surf)      # Draw the blue-yellow colorblind button.
+        total_btn.draw(12, main_surf)   # Draw the total colorblind button.
+        glasses_btn.draw(12, main_surf) # Draw the total colorblind button.
+        myopia_btn.draw(12, main_surf)  # Draw the red-green colorblind button.
+        hyperopia_btn.draw(12, main_surf)   # Draw the red-green colorblind button.
 
         pygame.display.update()
 
@@ -113,4 +146,61 @@ if __name__ == '__main__':
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if upload_btn.is_hovered(mouse_pos):
                     # Get the modified image and show it onto the screen.
-                    upload_btn_handler(main_surf)
+                    upload_btn_handler(screen)
+
+                # For colorblind buttons
+                elif rg_btn.is_hovered(mouse_pos):  # Red Green button listener
+                    rg_btn.setColor((67,168,94))
+                    rg_btn.setSelect(True)
+                        # Disable total colorblindness
+                    total_btn.setColor((139,158,144))
+                    total_btn.setSelect(False)
+                    """TODO: APPLY red-green colorblindness here"""
+
+                elif by_btn.is_hovered(mouse_pos):  # Blue Yellow button listener
+                    by_btn.setColor((67,168,94))
+                    by_btn.setSelect(True)
+                        # Disable total colorblindness
+                    total_btn.setColor((139,158,144))
+                    total_btn.setSelect(False)
+                    """TODO: APPLY blue yellow colorblindness here"""
+
+                elif total_btn.is_hovered(mouse_pos):  # Total button listener
+                    total_btn.setColor((67,168,94))
+                    total_btn.setSelect(True)
+                        # Disable red-green and blue-yellow colorblindness
+                    rg_btn.setColor((139,158,144))
+                    rg_btn.setSelect(False)
+                    by_btn.setColor((139,158,144))
+                    by_btn.setSelect(False)
+                    """TODO: APPLY total colorblindness here"""
+
+                # For refractive buttons
+                elif myopia_btn.is_hovered(mouse_pos):  # Myopia button listener
+                    myopia_btn.setColor((67,168,94))
+                    myopia_btn.setSelect(True)
+                        # Disable hyperopia colorblindness
+                    hyperopia_btn.setColor((139,158,144))
+                    hyperopia_btn.setSelect(False)
+                    """TODO: APPLY myopia here"""
+                
+                elif hyperopia_btn.is_hovered(mouse_pos):  # Hyperopia button listener
+                    hyperopia_btn.setColor((67,168,94))
+                    hyperopia_btn.setSelect(True)
+                        # Disable myopia colorblindness
+                    myopia_btn.setColor((139,158,144))
+                    myopia_btn.setSelect(False)
+                    """TODO: APPLY hyperopia here"""
+
+                elif glasses_btn.is_hovered(mouse_pos):  # Hyperopia button listener
+                    glasses_btn.setColor((67,168,94))
+                    glasses_btn.setSelect(True)
+                    glasses_img = pygame.image.load("images/glasses.png")
+                    screen.blit(glasses_img, (60, 80))
+                    """TODO: APPLY glasses here (correct colorblind or myopia etc.)"""
+            
+            # Right click
+            """TODO: UNDO any pressed button"""
+
+            # Key press
+            """TODO: Change the values of myopia and hyperopia, have a pop-up to display the values"""
